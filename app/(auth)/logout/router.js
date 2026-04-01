5e60c3a1fcf04545efcde02c08daf2b58a159557
unknown <debkanta.starwaywebdigital@gmail.com>
2026-04-01 06:50:42 -0700

    solve production problem

--- app/(auth)/logout/router.js
+++ app/(auth)/logout/router.js
@@ -11,7 +11,7 @@ export async function POST() {
   response.cookies.set("token", "", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "lax",
     maxAge: 0,
     path: "/",
   });
--- app/layout.jsx
+++ app/layout.jsx
@@ -25,7 +25,6 @@ export default function RootLayout({ children }) {
           href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
           rel="stylesheet"
         />
-
         <link
           href="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.css"
           rel="stylesheet"
@@ -36,13 +35,18 @@ export default function RootLayout({ children }) {
       >
         {children}
 
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-        <script src="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.js"></script>
-
-        <script type="module" src="/src/main.jsx"></script>
+        <Script
+          src="https://code.jquery.com/jquery-3.5.1.min.js"
+          strategy="beforeInteractive"
+        />
+        <Script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
+          strategy="beforeInteractive"
+        />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.js"
+          strategy="beforeInteractive"
+        />
       </body>
     </html>
   );
